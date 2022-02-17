7e1c6b503062399ea39b2419da7d6f1e942ed391
Shmeigo <54638093+Shmeigo@users.noreply.github.com>
2022-02-16 20:59:01 -0500

    Added camera following player, placeholder map layout, jump, slight modifications to other code

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 6f;
+    float jumpSpeed = 5f;
+    float camSpeed = 3f;
+    float maxCamDist = 1f;
+    public Rigidbody rb;
+    bool canJump = false;
+    Vector3 camOffset = new Vector3(-10f, 10f, -10f);
+
+    Vector3 forward, right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Changed this because the camera's forward is pointing slightly down
+        // forward = Camera.main.transform.forward;
+        // forward.y = 0f;
+        // // make sure length is one
+        // forward = Vector3.Normalize(forward);
+        forward = Quaternion.Euler(new Vector3(0, 45, 0)) * Vector3.forward;
+        right = Quaternion.Euler(new Vector3(0, 45, 0)) * Vector3.right;
+        Camera.main.transform.position = transform.position + camOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            Move();
+        }
+        MoveCamera();
+    }
+
+    void Move()
+    {
+        //Was previously only normalizing the forward transform and not the actual movement
+        Vector3 direction = Vector3.Normalize(new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey")));
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * direction.x;
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * direction.z;
+        // Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        // Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        // movement heading
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+
+    void Jump() {
+        if (canJump) {
+            rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Ground") {
+            canJump = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision) {
+        if (collision.collider.tag == "Ground") {
+            canJump = false;
+        }
+    }
+
+    void MoveCamera() {
+        Vector3 start = Camera.main.transform.position;
+        Vector3 dest = transform.position + camOffset;
+        Vector3 dir = start - dest;
+
+        float dist = dir.magnitude;
+        float step = (dist / maxCamDist) * camSpeed;
+
+        Camera.main.transform.position -= dir.normalized * step * Time.deltaTime;
+    }
+}
