388b1e765f439c5aa47686194ffe5176fe68cea1
Angela Zhang <ayzhang@andrew.cmu.edu>
2022-02-18 12:45:35 -0500

    light up works with attack

--- Assets/Scripts/LightUp.cs
+++ Assets/Scripts/LightUp.cs
@@ -7,16 +7,16 @@ public class LightUp : MonoBehaviour
     public Material M1;
     public Material M2;
     bool isLitUp = false;
-    void OnMouseDown()
-    {
-        if (isLitUp) {
-            GetComponent<MeshRenderer>().material = M1;
-            isLitUp = false;
+    void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "Attack") {
+            if (isLitUp) {
+                GetComponent<MeshRenderer>().material = M1;
+                isLitUp = false;
+            }
+            else {
+                GetComponent<MeshRenderer>().material = M2;
+                isLitUp = true;
+            }
         }
-        else {
-            GetComponent<MeshRenderer>().material = M2;
-            isLitUp = true;
-        }
-        
     }
 }
