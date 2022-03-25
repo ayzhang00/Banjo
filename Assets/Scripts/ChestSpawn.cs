72626b0b4b734d7eddc5b28bc10a280147ac5ff8
Shmeigo <54638093+Shmeigo@users.noreply.github.com>
2022-03-25 13:04:34 -0400

    chest spawns when all lights activated

--- Assets/Scripts/ChestSpawn.cs
+++ Assets/Scripts/ChestSpawn.cs
@@ -13,11 +13,14 @@ public class ChestSpawn : MonoBehaviour
     void Update() {
         if (isNotSpawned) {
             // chest drops when all leds lit up
-            if (LED1.GetComponent<LightUp>().isLitUp &&
-                LED2.GetComponent<LightUp>().isLitUp &&
-                LED3.GetComponent<LightUp>().isLitUp) {
+            // if (LED1.GetComponent<LightUp>().isLitUp &&
+            //     LED2.GetComponent<LightUp>().isLitUp &&
+            //     LED3.GetComponent<LightUp>().isLitUp) {
+            if (LED1.active &&
+                LED2.active &&
+                LED3.active) {
                 if (PhotonNetwork.IsMasterClient) {
-                    PhotonNetwork.Instantiate("Chest", new Vector3(24f, 2f, 20f), Quaternion.identity, 0);
+                    PhotonNetwork.Instantiate("Chest", new Vector3(5f, 5f, 12f), Quaternion.identity, 0);
                     isNotSpawned = false;
                 }
             }
