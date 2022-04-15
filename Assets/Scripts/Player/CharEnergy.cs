3db43bfe3bbf5f0a5595577862d3902412aaae4d
Angela Zhang <ayzhang@andrew.cmu.edu>
2022-04-15 05:48:43 -0400

    hold r to recharge

--- Assets/Scripts/Player/CharEnergy.cs
+++ Assets/Scripts/Player/CharEnergy.cs
@@ -7,10 +7,12 @@ public class CharEnergy : MonoBehaviour
 {
     public int energy = 4;
     public GameObject batteryUI;
+    public GameObject RechargeNotif;
     public Sprite[] batterySprite;
     float timeRecharged = 0f;
     Image batteryImage;
     bool recharging = false;
+    bool canRecharge = false;
 
     void Start(){
         batteryImage = batteryUI.GetComponent<Image>();
@@ -20,24 +22,37 @@ public class CharEnergy : MonoBehaviour
         if (!recharging) {
             UpdateBattery(false);
         }
+        if (canRecharge && Input.GetButtonDown("Recharge")) {
+            recharging = true;
+            Recharge();
+        }
+        if (Input.GetButtonUp("Recharge")) {
+            timeRecharged = 0;
+            recharging = false;
+        }
+
+        if (canRecharge && !recharging && energy < 4) {
+            DisplayRecharge(true);
+        }
+        else {
+            DisplayRecharge(false);
+        }
+
+        if (recharging) {
+            Recharge();
+        }
     }
 
     private void OnTriggerStay(Collider other) {
-        if (other.tag == "Recharge" && energy <= 4) {
-            recharging = true;
-            if (energy == 0) UpdateBattery(true);
-            else UpdateBattery(false);
-            timeRecharged += Time.deltaTime;
-            if (timeRecharged >= 5.0f && energy < 4) {
-                timeRecharged = 0;
-                energy++;
-            }
+        if (other.tag == "Recharge" && energy < 4) {
+            canRecharge = true;
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.tag == "Recharge") {
             timeRecharged = 0;
+            canRecharge = false;
             recharging = false;
         }
     }
@@ -54,6 +69,20 @@ public class CharEnergy : MonoBehaviour
     public void DecEnergy() {
         if (energy > 0) {
             energy--;
+        }
+    }
+
+    private void DisplayRecharge(bool r) {
+        RechargeNotif.SetActive(r);
+    }
+
+    private void Recharge() {
+        if (energy == 0) UpdateBattery(true);
+        else UpdateBattery(false);
+        timeRecharged += Time.deltaTime;
+        if (timeRecharged >= 5.0f && energy < 4) {
+            timeRecharged = 0;
+            energy++;
         }
     }
 }
