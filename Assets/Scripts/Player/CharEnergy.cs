0d5894474fac15041485d181113fb46d62468781
Angela Zhang <ayzhang@andrew.cmu.edu>
2022-04-21 21:08:02 -0400

    energy + boundaries

--- Assets/Scripts/Player/CharEnergy.cs
+++ Assets/Scripts/Player/CharEnergy.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CharEnergy : MonoBehaviour
-{
+{   
+    public GameObject LoadingUI;
+    public Sprite[] LoadingSprites;
     public int energy = 4;
     public GameObject batteryUI;
     public GameObject RechargeNotif;
     public Sprite[] batterySprite;
+    public float totalTime = 5f;
     float timeRecharged = 0f;
     Image batteryImage;
     bool recharging = false;
     bool canRecharge = false;
     CharController c;
+    Image loading;
+    
+    public int loadingNum = 9;
+    int currLoading = 0;
 
     void Start(){
         batteryImage = batteryUI.GetComponent<Image>();
         c = GetComponent<CharController>();
+        loading = LoadingUI.GetComponent<Image>();
     }
 
     void Update() {
+        HandleUI();
         if (!recharging) {
             UpdateBattery(false);
         }
@@ -92,9 +101,23 @@ public class CharEnergy : MonoBehaviour
         if (timeRecharged >= 5.0f && energy < 4) {
             timeRecharged = 0;
             energy = 4;
+            recharging = false;
         }
         if (energy >= 4) {
             canRecharge = false;
+        }
+    }
+    private void HandleUI() {
+        if (recharging) {
+            LoadingUI.SetActive(true);
+            CalculateLoading();
+            loading.sprite = LoadingSprites[currLoading];
+        }
+    }
+    private void CalculateLoading() {
+        float inc = totalTime / loadingNum;
+        if (timeRecharged >= ((currLoading + 1)*inc) && currLoading < 9) {
+            currLoading++;
         }
     }
 }
